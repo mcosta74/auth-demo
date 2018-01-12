cf137cab9baa113ad6f0fa264ee5504a4a3effa2
Massimo Costa <massimoc@filewave.com>
2018-01-12 16:04:04 +0100

    HELLO

--- auth_demo/auth_demo/settings.py
+++ auth_demo/auth_demo/settings.py
@@ -121,6 +121,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+LOCALE_PATHS = [
+	'/tmp/translations'
+]
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
